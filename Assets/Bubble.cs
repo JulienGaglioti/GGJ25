cf9c838137420f20ca47822f545562023ce2af29
Guido Belluomo <guidobelluomo@gmail.com>
2025-01-25 12:24:47 +0100

    BubbleCollisionMerger.cs

--- Assets/Bubble.cs
+++ Assets/Bubble.cs
@@ -1,21 +1,48 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Bubble : MonoBehaviour
 {
+    public int priority = 1;
+    public enum MergePosition
+    {
+        Midpoint,
+        SelfOrigin
+    }
+    
     private static readonly int RadiusProperty = Shader.PropertyToID("_Radius");
-    [SerializeField] protected float value = 1f;
+
+    [SerializeField] private float value;
+    protected float Value
+    {
+        get => value;
+        private set
+        {
+            this.value = value;
+            transform.localScale = Vector3.one * (float)Math.Max(0, Math.Log(Math.Max(0, this.value) + 1) * multiplier);
+            _meshRenderer.material.SetFloat(RadiusProperty, transform.localScale.x / 2);
+        }
+    }
+    
     [SerializeField] protected float multiplier = 1f;
     private MeshRenderer _meshRenderer;
 
     private void Awake()
     {
         _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        Value = value;
     }
 
-    private void Update()
+    public void MergeWith(Bubble bubble, MergePosition mergePosition = MergePosition.Midpoint)
     {
-        transform.localScale = Vector3.one * (float)Math.Max(0, Math.Log(Math.Max(0, value) + 1) * multiplier);
-        _meshRenderer.material.SetFloat(RadiusProperty, transform.localScale.x / 2);
+        if (mergePosition == MergePosition.Midpoint)
+        {
+            transform.position = (bubble.transform.position + transform.position) / 2;
+        }
+
+        Value += bubble.Value;
+        Destroy(bubble.gameObject);
     }
 }
---
+++ Assets/BubbleCollisionMerger.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class BubbleCollisionMerger : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.gameObject.CompareTag("Bubble")) return;
+        
+        var bubble = GetComponent<Bubble>();
+        var otherBubble = other.gameObject.GetComponent<Bubble>();
+        if (bubble.priority > otherBubble.priority)
+        {
+            bubble.MergeWith(otherBubble);
+        }
+        else if (bubble.priority == otherBubble.priority)
+        {
+            if (bubble.GetInstanceID() > otherBubble.GetInstanceID())
+            {
+                bubble.MergeWith(otherBubble);
+            }
+        }
+    }
+}
