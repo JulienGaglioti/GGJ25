435a616e14e84147ddc4c8944707f777ed9e24fa
Guido Belluomo <guidobelluomo@gmail.com>
2025-01-25 14:18:50 +0100

    Acceleration and deceleration

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -1,46 +1,68 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(PlayerInput))]
 public class Movement2D : MonoBehaviour
 {
     [Serializable] public class MoveEvent : UnityEvent<Vector2> {}
 
+    [Header("Movement Settings")]
+    [SerializeField] private float maxSpeed = 2f;
+    [SerializeField] private float acceleration = 4f;
+    [SerializeField] private float deceleration = 4f;
+
     public bool CanMove { get; set; } = true;
+
     public MoveEvent OnMove;
-    public Vector2 Velocity => _velocity;
-    public Vector2 NormalizedVelocity => _normalizedVelocity;
+
+    public Vector2 Velocity => _currentVelocity;
+    public Vector2 NormalizedVelocity => _currentVelocity.normalized;
     public BoxCollider2D Collider => _collider;
     public Rigidbody2D Rigidbody => _rigidbody;
 
-    private Vector2 _velocity;
-    private Vector2 _normalizedVelocity;
+    private Vector2 _currentVelocity;
+    private Vector2 _targetVelocity;
+
     private BoxCollider2D _collider;
     private Rigidbody2D _rigidbody;
-        
 
-    [SerializeField] protected float speedMultiplier = 5;
-
-    // Start is called before the first frame update
-    void Awake()
+    private void Awake()
     {
         _collider = GetComponent<BoxCollider2D>();
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    public void Move(Vector2 direction)
+    private void Update()
     {
-        _velocity = direction * speedMultiplier;
-        _normalizedVelocity = _velocity.normalized;
-        OnMove.Invoke(_velocity);
+        if (CanMove)
+        {
+            Move(InputManager.Instance.MoveInput);
+        }
+        else
+        {
+            Move(Vector2.zero);
+        }
     }
 
-    public void FixedUpdate()
+    public void Move(Vector2 direction)
     {
-        Rigidbody.linearVelocity = Velocity;
+        _targetVelocity = direction.normalized * maxSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        float currentAcceleration = _targetVelocity.sqrMagnitude > 0.0001f ? acceleration : deceleration;
+
+        _currentVelocity = Vector2.MoveTowards(
+            _currentVelocity,
+            _targetVelocity,
+            currentAcceleration * Time.fixedDeltaTime
+        );
+
+        Rigidbody.linearVelocity = _currentVelocity;
+
+        OnMove.Invoke(_currentVelocity);
     }
 }
